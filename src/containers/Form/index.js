3e4cbaa53cb38450d18c3dbb738b87a7fb2995a5
loc bernard <locbernard35@gmail.com>
2024-03-24 00:41:14 +0100

    add from

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -4,18 +4,24 @@ import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
 import Button, { BUTTON_TYPES } from "../../components/Button";
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 1000); })
+const mockContactApi = () =>
+  new Promise((resolve) => {
+    setTimeout(resolve, 1000);// 5000 debug timeout
+  });
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
+
   const sendContact = useCallback(
     async (evt) => {
       evt.preventDefault();
+
       setSending(true);
       // We try to call mockContactApi
       try {
         await mockContactApi();
         setSending(false);
+        onSuccess(true); // Add onSuccess 
       } catch (err) {
         setSending(false);
         onError(err);
@@ -31,7 +37,6 @@ const Form = ({ onSuccess, onError }) => {
           <Field placeholder="" label="Prénom" />
           <Select
             selection={["Personel", "Entreprise"]}
-            onChange={() => null}
             label="Personel / Entreprise"
             type="large"
             titleEmpty
@@ -56,11 +61,11 @@ const Form = ({ onSuccess, onError }) => {
 Form.propTypes = {
   onError: PropTypes.func,
   onSuccess: PropTypes.func,
-}
+};
 
 Form.defaultProps = {
   onError: () => null,
   onSuccess: () => null,
-}
+};
 
 export default Form;
